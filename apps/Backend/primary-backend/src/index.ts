1e391a4f915e3144d75a7be5a6d98f39d30de9ac
chisty <mchisty31@gmail.com>
2024-08-06 15:54:27 +0600

    primary hook added

--- apps/Backend/primary-backend/src/index.ts
+++ apps/Backend/primary-backend/src/index.ts
@@ -1,3 +1,4 @@
+import { prisma } from './../../../../packages/database/src/client';
 import express, { Request, Response } from 'express';
 
 const app = express();
@@ -8,6 +9,35 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, TypeScript with Express!');
 });
+
+
+
+// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
+// password logic
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body;
+
+    // store in db a new trigger
+    await prisma.$transaction(async tx => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body
+            }
+        });;
+
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+    res.json({
+        message: "Webhook received"
+    })
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
