4465c0fa33d809a8964513de27d0045f00209ce4
chisty <mchisty31@gmail.com>
2024-08-07 22:30:20 +0600

    frontend and primary be setup

--- apps/Backend/primary-backend/src/index.ts
+++ apps/Backend/primary-backend/src/index.ts
@@ -1,44 +1,14 @@
-import { prisma } from './../../../../packages/database/src/client';
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const port = 3000;
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
+  res.send('Hello, World!');
 });
 
-
-
-// https://hooks.zapier.com/hooks/catch/17043103/22b8496/
-// password logic
-app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
-    const userId = req.params.userId;
-    const zapId = req.params.zapId;
-    const body = req.body;
-
-    // store in db a new trigger
-    await prisma.$transaction(async tx => {
-        const run = await tx.zapRun.create({
-            data: {
-                zapId: zapId,
-                metadata: body
-            }
-        });;
-
-        await tx.zapRunOutbox.create({
-            data: {
-                zapRunId: run.id
-            }
-        })
-    })
-    res.json({
-        message: "Webhook received"
-    })
-})
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
