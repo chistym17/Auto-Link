59d63bb7117e509956cce1db0b028487ed1cf403
chisty <mchisty31@gmail.com>
2024-08-14 14:12:35 +0600

    hero section updated

--- apps/frontend/components/Appbar.tsx
+++ apps/frontend/components/Appbar.tsx
@@ -6,14 +6,14 @@ import { PrimaryButton } from "./buttons/PrimaryButton";
 export const Appbar = () => {
     const router = useRouter();
     return <div className="flex border-b justify-between p-4">
-        <div className="flex flex-col justify-center text-2xl font-extrabold">
-            Zapier
+        <div className="flex flex-col justify-center text-2xl text-pink font-extrabold">
+            IntegrateIT
         </div>
-        <div className="flex">
-            <div className="pr-4">
+        <div className="flex text-xl">
+            <div className="pr-4 text-orange">
                 <LinkButton onClick={() => {}}>Contact Sales</LinkButton>
             </div>
-            <div className="pr-4">
+            <div className="pr-4 text-orange">
                 <LinkButton onClick={() => {
                     router.push("/login")
                 }}>Login</LinkButton>
